c119835da72bfe9d74ba15f0c36eb0abca8d67a3
Zach Overman <zvoverman@gmail.com>
2025-01-23 13:50:28 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;  // You can choose any available port
+
+// Serve static files from the 'assets' directory (or any folder you specify)
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Default route (optional)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
