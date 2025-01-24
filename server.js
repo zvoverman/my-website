02349145a416f82fb4888cf12208ec6246d1e7c0
Zach Overman <zvoverman@gmail.com>
2025-01-23 21:07:56 -0700

    server change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 3000;  // You can choose any available port
 
 // Serve static files from the 'assets' directory (or any folder you specify)
-app.use(express.static(path.join(__dirname, 'docs')));
+app.use(express.static(path.join(__dirname, '')));
 
 // Default route (optional)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'docs/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
