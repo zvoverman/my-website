99b306648c4225e8f64f70eb78c8cc7082c75eb5
Zach Overman <zvoverman@gmail.com>
2025-01-23 20:57:52 -0700

    add links + new dev routes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 3000;  // You can choose any available port
 
 // Serve static files from the 'assets' directory (or any folder you specify)
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // Default route (optional)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/index.html'));
+    res.sendFile(path.join(__dirname, 'docs/index.html'));
 });
 
 // Start the server
